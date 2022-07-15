936cfaa5e5f927e8cdcbd3be6d8c7adb3a37a42e
Sammy <sammyntewusu1@gmail.com>
2022-07-15 21:53:01 +0000

    made some changes

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const menuOpen = document.getElementById("menu-open")
 const menuClose = document.getElementById("menu-close")
 const mobileMenu = document.querySelector(".mobile-menu")
-const logo = document.querySelector(".mobile-sn                         ")
-const header = document.querySelector("header")
+const logo = document.querySelector(".mobile-sn")
 
 function openMenu(){
     mobileMenu.style.display = "block"
     menuOpen.style.display = "none"
     menuClose.style.display = "block"
+    menuClose.style.color = "white"
     logo.style.color = "white"
 }
 
@@ -23,16 +23,67 @@ menuOpen.onclick = openMenu
 menuClose.onclick = closeMenu
 
 const typed = new Typed(".typing",{
-    strings: ["First, solve the problem. Then, write the code.", "“Website without visitors is like a ship lost in the horizon -Dr. Christopher Dayagdag", "hey"],
+    strings: ["First, solve the problem. Then, write the code.", "“A website without visitors is like a ship lost in the horizon -Dr. Christopher Dayagdag"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
 })
 
-function mywork(){
+function myWork(){
     window.location.href = "./mywork.html"
 }
 
-function hireMe(){
-    window.location.href = "mailto: sammyntewusu1@gmail.com"
+function aboutMe(){
+    window.location.href = "./about.html"
 }
+
+function siteupdate(){
+    document.getElementById("warning-display").style.display = 'block';
+}
+
+function closeSiteUpdate(){
+    document.getElementById("warning-display").style.display = 'none';
+}
+
+document.querySelector(".mywork-btn").addEventListener('mouseover', ()=>{
+    document.querySelector(".aboutme-btn").style.background = "var(--primary-color)";
+    document.querySelector(".aboutme-btn").style.transition = "all 350ms ease";
+    document.querySelector(".aboutme-btn").style.border = "none";
+    document.querySelector(".aboutme-btn").style.color = "white";
+    document.querySelector(".mywork-btn").style.background = "white";
+    document.querySelector(".mywork-btn").style.transition = "all 350ms ease";
+    document.querySelector(".mywork-btn").style.color = "var(--primary-color)";
+})
+
+document.querySelector(".mywork-btn").addEventListener('mouseleave', ()=>{
+    document.querySelector(".aboutme-btn").style.background = "white";
+    document.querySelector(".aboutme-btn").style.border = "1px solid var(--primary-color)";
+    document.querySelector(".aboutme-btn").style.transition = "all 350ms ease";
+    document.querySelector(".aboutme-btn").style.color = "var(--primary-color)";
+    document.querySelector(".mywork-btn").style.background = "var(--primary-color)";
+    document.querySelector(".mywork-btn").style.transition = "all 350ms ease";
+    document.querySelector(".mywork-btn").style.color = "white";
+
+})
+
+document.querySelector(".aboutme-btn").addEventListener('mouseover', ()=>{
+    document.querySelector(".aboutme-btn").style.background = "var(--primary-color)";
+    document.querySelector(".aboutme-btn").style.transition = "all 350ms ease";
+    document.querySelector(".aboutme-btn").style.border = "none";
+    document.querySelector(".aboutme-btn").style.color = "white";
+    document.querySelector(".mywork-btn").style.background = "white";
+    document.querySelector(".mywork-btn").style.border = "1px solid var(--primary-color)";
+    document.querySelector(".mywork-btn").style.transition = "all 350ms ease";
+    document.querySelector(".mywork-btn").style.color = "var(--primary-color)";
+})
+
+document.querySelector(".aboutme-btn").addEventListener('mouseleave', ()=>{
+    document.querySelector(".aboutme-btn").style.background = "white";
+    document.querySelector(".aboutme-btn").style.border = "1px solid var(--primary-color)";
+    document.querySelector(".aboutme-btn").style.transition = "all 350ms ease";
+    document.querySelector(".aboutme-btn").style.color = "var(--primary-color)";
+    document.querySelector(".mywork-btn").style.background = "var(--primary-color)";
+    document.querySelector(".mywork-btn").style.transition = "all 350ms ease";
+    document.querySelector(".mywork-btn").style.color = "white";
+
+})
--- projects/face_animation/index.js
+++ projects/face_animation/index.js
@@ -1,19 +1,21 @@
-smiley = document.querySelector(".smily-face");
-sad = document.querySelector(".sad-face");
-suprised = document.querySelector(".suprised-face");
-smileyImage = document.getElementById("smiley-img");
-suprisedImage = document.getElementById("suprised-img");
-sadImage = document.getElementById("sad-img");
-color1 = document.getElementById("color1");
-color2 = document.getElementById("color2");
-color3 = document.getElementById("color3");
-facesContainer = document.getElementById("faces");
-smilyFace = document.getElementById("smily-face");
-suprisedFace = document.getElementById("suprised-face");
-sadFace = document.getElementById("sad-face");
-faceColor1 = document.getElementById("face-color1");
-faceColor2 = document.getElementById("face-color2");
-faceColor3 = document.getElementById("face-color3");
+const smiley = document.querySelector(".smily-face");
+const sad = document.querySelector(".sad-face");
+const suprised = document.querySelector(".suprised-face");
+const smileyImage = document.getElementById("smiley-img");
+const suprisedImage = document.getElementById("suprised-img");
+const sadImage = document.getElementById("sad-img");
+const color1 = document.getElementById("color1");
+const color2 = document.getElementById("color2");
+const color3 = document.getElementById("color3");
+const facesContainer = document.getElementById("faces");
+const smilyFace = document.getElementById("smily-face");
+const suprisedFace = document.getElementById("suprised-face");
+const sadFace = document.getElementById("sad-face");
+const faceColor1 = document.getElementById("face-color1");
+const faceColor2 = document.getElementById("face-color2");
+const faceColor3 = document.getElementById("face-color3");
+const input1 = document.querySelector('#background-change')
+const input2 = document.querySelector('#face-color-change')
 
 function smileShow(){
     smiley.style.display = 'block';
@@ -62,6 +64,18 @@ function faceColorChange3(){
     suprisedFace.style.background = 'rgb(107, 106, 42)';
     sadFace.style.background = 'rgb(107, 106, 42)';
 }
+
+input1.addEventListener('keyup', e =>{
+    const text = e.target.value
+    facesContainer.style.background = text;
+})
+
+input2.addEventListener('keyup', e =>{
+    const text = e.target.value
+    smilyFace.style.background = text;
+    suprisedFace.style.background = text;
+    sadFace.style.background = text;
+})
 
 smileyImage.onclick = smileShow;
 suprisedImage.onclick = suprisedShow;
